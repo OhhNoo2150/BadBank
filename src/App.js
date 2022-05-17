9bc159b03e773afa5798bdaf7312b7cd353d939b
Daniel Rivera <daniel.i.rivera@protonmail.com>
2022-05-16 20:14:01 -0500

    updated to HashRouter from BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home.js';
 import CreateAccount from './pages/CreateAccount';
 import Withdraw from './pages/Withdraw';
